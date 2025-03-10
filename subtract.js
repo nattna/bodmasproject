4b37246bfcc5e6137e4c0f66e14e6c82c20b35ad
nattna <136722304+nattna@users.noreply.github.com>
2025-03-10 11:30:58 +0000

    subtraction show answer

---
+++ subtract.js
@@ -0,0 +1,30 @@
+function loadAnswer_16() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+  }
+
+function loadAnswer_17() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+  }
+
+function loadAnswer_18() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+  }
+  
