a2050467a679ec0628ea81bc378db8dc18feb688
nattna <136722304+nattna@users.noreply.github.com>
2025-04-02 21:29:21 +0100

    FINAL

--- order.js
+++ order.js
@@ -1,30 +1,24 @@
-function loadAnswer_4() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-    xhttp.open("GET", "ajax_info.txt");
-    xhttp.send();
-  }
+//Function that changes the elements content
+//document.getElementById("hiddenAnswer(which ever number is the function)") means HTML element found by id from the HTML code 
+//.style.display means we check the HTML element's style and display property
+//"block" means that the element becomes visible
+function revealAnswer_4() {
+    document.getElementById("hiddenAnswer4").style.display = "block"
+}
 
-function loadAnswer_5() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-    xhttp.open("GET", "ajax_info.txt");
-    xhttp.send();
-  }
+function revealAnswer_5() {
+    document.getElementById("hiddenAnswer5").style.display = "block"
+}
 
-function loadAnswer_6() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-    xhttp.open("GET", "ajax_info.txt");
-    xhttp.send();
-  }
+function revealAnswer_6() {
+    document.getElementById("hiddenAnswer5").style.display = "block"
+}
+//document.getElementById("home1") means HTML element found by id from the HTML code
+//.addEventListener("click",...) attaches a click event listener to ‘home1’ when the button is pressed 
+//() => { history.back(); } executes the function to go back to the home page when the element is clicked 
+document.getElementById("home1").addEventListener("click", () => {
+    history.back();
+  });
+
   
+
