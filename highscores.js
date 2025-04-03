add7ecc3d1624a790ab5a4a58e53f3c4a30f1e38
nattna <136722304+nattna@users.noreply.github.com>
2025-04-03 15:27:29 +0100

    highscores.js

--- highscores.js
+++ highscores.js
@@ -4,7 +4,7 @@ const sessions = require('express-session');
 const http = require('http');
 var parseUrl = require('body-parser');
 const app = express();
-const path = require('path');
+const path = require('path');//allows a path so that foreign keys can be accessed
 
 var mysql = require('mysql2');
 
@@ -27,7 +27,7 @@ var con = mysql.createConnection({
     password: "zaqwsxcde127", // my password
     database: "leaderBoard"
 });
-
+//gets the top 10 and their username and scores and ranks them from 1st to 10th
 app.get('/leaderboard', (req, res) => {
     con.query('SELECT username, score FROM leader_board ORDER BY score DESC LIMIT 10', function(err, result) {
         res.json(result);
@@ -36,14 +36,14 @@ app.get('/leaderboard', (req, res) => {
 
 app.post('/top10', encodeUrl, (req, res) => {
     const username = req.body.username;
-
+//gets the users searched username from the database 
     con.query('SELECT score FROM leader_board WHERE username = ${username} ORDER BY score DESC', [username], function(err, result) {
         if (result.length === 0) {
             return res.send("this username isn't in the leaderboard");
         }
 
         const score = result[0].score;
-
+//gets the searched users ranking from the database 
         con.query('SELECT COUNT(*) AS rank FROM leader_board WHERE score > ${score}', [score], function(err, topten)
         {
             const rank = rankResult[0].rank + 1; 
